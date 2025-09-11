5e73174aeb532b3ead85006eceb39c3b6b755fcd
Dharshik <dharshik06@gmail.com>
2025-09-11 20:00:19 +0530

    File manipulation

--- lua/raven/core/keymaps.lua
+++ lua/raven/core/keymaps.lua
@@ -1,9 +1,30 @@
 vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { desc = "Save file (insert mode)" })
 
-vim.keymap.set("n", "<F9>", function()
-    vim.cmd("!gcc % -o %:r && ./%:r")
-end, { desc = "Compile & run C file" })
+vim.keymap.set("n", "<leader>n", "<cmd>ene | startinsert<CR>")
+
+vim.keymap.set("n", "<leader>r", function()
+  local old_name = vim.fn.expand("%:p")
+  local new_name = vim.fn.input("Rename to: ", old_name, "file")
+  if new_name ~= "" and new_name ~= old_name then
+    vim.cmd("saveas " .. new_name)
+    vim.fn.delete(old_name)
+    vim.cmd("bdelete " .. vim.fn.bufnr(old_name))
+end
+end, { desc = "Rename current file" })
+
+vim.keymap.set("n", "<leader>d", function()
+    local file_dir = vim.fn.expand("%:p:h")
+    local file_name = vim.fn.input("Delete file: ", file_dir, "file")
+    if file_name ~= "" then
+        local ok = vim.fn.delete(file_name)
+    if ok == 0 then
+        print("Deleted: " .. file_name)
+    else
+        print("Failed to delete: " .. file_name)
+    end
+  end
+end, { desc = "Delete file by name" })
 
 vim.keymap.set("n", "<F10>", ":!python %<CR>", { desc = "Run Python file" })
 
